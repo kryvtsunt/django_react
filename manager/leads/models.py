f347c1cffd0db29093a72d40e54af248d1553ed8
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-09-24 11:07:23 -0400

    simple lead model

--- manager/leads/models.py
+++ manager/leads/models.py
@@ -6,3 +6,4 @@ class Lead(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, black=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
