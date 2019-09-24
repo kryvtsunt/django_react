1217e01ae6f2166d93bbcc37b35bb0e8e9a464e1
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-09-24 16:40:32 -0400

    redux set up, get and delete reducers

--- manager/leads/urls.py
+++ manager/leads/urls.py
@@ -1,11 +1,10 @@
 from rest_framework import routers
 from .api import LeadViewSet
+from django.urls import path, include
+from django.conf.urls import url
 
-# router = routers.DefaultRouter()
-# router.register('api/leads', LeadViewSet, 'leads')
 
-# urlpatterns = router.urls
+router = routers.DefaultRouter()
+router.register('leads', LeadViewSet, 'leads')
 
-urlpatterns = [
-    path('api/leads', LeadViewSet)
-]
+urlpatterns = router.urls
--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('frontend.urls')),
-    path('', include('leads.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('leads.urls')),
+    path('', include('frontend.urls')),
 ]
