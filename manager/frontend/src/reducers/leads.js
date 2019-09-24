86b88f107e2fe64bf1fe4baca7030466f0d3145d
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-09-24 16:59:55 -0400

    add lead reducer

--- manager/frontend/src/reducers/leads.js
+++ manager/frontend/src/reducers/leads.js
@@ -1,4 +1,9 @@
-import { GET_LEADS, DELETE_LEAD } from "../actions/types.js";
+import {
+  GET_LEADS,
+  DELETE_LEAD,
+  ADD_LEAD,
+  CLEAR_LEADS
+} from "../actions/types.js";
 
 const initialState = {
   leads: []
@@ -15,6 +20,16 @@ export default function(state = initialState, action) {
       return {
         ...state,
         leads: state.leads.filter(lead => lead.id !== action.payload)
+      };
+    case ADD_LEAD:
+      return {
+        ...state,
+        leads: [...state.leads, action.payload]
+      };
+    case CLEAR_LEADS:
+      return {
+        ...state,
+        leads: []
       };
     default:
       return state;
